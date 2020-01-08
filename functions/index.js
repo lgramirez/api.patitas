2ef0f5d5a00dd1cf71b0e402eb34ccfa20a28cd3
lgramirez <gramirez.developer@gmail.com>
2020-01-08 16:06:45 -0400

    Expose API to get pets

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const config = require('./firebase-config.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+firebase.initializeApp({
+    credential: firebase.credential.cert(config),
+    databaseURL: 'https://patitas-9da3d.firebaseio.com'
+});
+
+exports.api = functions.https.onRequest((req, res) => {
+    res.header('Content-Type', 'application/json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    const data = firebase.database().ref('/pets');
+
+    if (req.method === 'GET') {
+        data.on('value', (snapshot) => {
+            const parseData = Object.keys(snapshot.val() || {}).map(key => snapshot.val()[key]);
+            res.json(parseData);
+        });
+    }
+});
